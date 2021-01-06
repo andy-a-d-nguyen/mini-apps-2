236fbb02e1bdca53c27a0b968196435b2a65a58c
andy-a-d-nguyen <a.d.nguyen.andy@gmail.com>
2021-01-06 11:59:42 -0800

    Created form. Received query results from server

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,43 @@
+import React from 'react';
+import { Form, Input, Button } from 'antd';
+import axios from 'axios';
 
+const App = () => {
+  const [input, setInput] = React.useState('');
+  const [results, setResults] = React.useState([]);
 
+  const getResults = () => {
+    axios.get('/events', {
+      params: {
+        q: input
+      }
+    })
+    .then(res => console.log(res))
+    .err(err => console.log(err));
+  }
+  return (
+    <Form
+      layout='inline'
+    >
+      <Form.Item
+        label='Keyword to Search'
+        name='keyword'
+        rules={[
+          {
+            required: true,
+            message: 'Please input a keyword to search for!',
+          },
+        ]}
+      >
+        <Input onChange={event => setInput(event.target.value)}/>
+      </Form.Item>
+      <Form.Item >
+        <Button type="primary" htmlType="submit" onClick={getResults}>
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
+
+export default App;
---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+
+};
